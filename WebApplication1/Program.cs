a18b5e84a8943d98149c95af0754f9f3a5331b7b
veivesh <veivesh95@gmail.com>
2020-02-13 19:09:34 +0530

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // creates the host to run itself (self running ASP applications)
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        // creates what kind of web server -> defaults to Kestrel Web server
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}

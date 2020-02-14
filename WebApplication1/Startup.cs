e08f3796bae6ee083d447a56ac34cb375c857ff4
veivesh <veivesh95@gmail.com>
2020-02-14 12:00:32 +0530

    Controller basics added

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -16,6 +16,7 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,12 +32,21 @@ namespace WebApplication1
 
             app.UseRouting();
 
+            // can also specify options to get a different directory
+            // app.UseStaticFiles(options => { new StaticFileOptions(){}});
+
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                // to specify how the path should be specified /controller/resource/params
+                endpoints.MapDefaultControllerRoute();
+
             });
 
             // app.Run(async context => await context.Response.WriteAsync("daosndo"));
